1f0a6277b0a3916d8eb5d9facc3c96b9143965ee
Jayden W <jaymanw2000@gmail.com>
2021-06-12 03:56:40 -0500

    nav bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,23 +7,28 @@ import {
 } from "react-router-dom";
 
 // COMPONENTS
-import Search from ''
+import Search from './components/Search'
+import Saved from './components/Saved'
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <nav>
-            <a>Google Books</a>
+          <nav className="menu">
+            <a href="/" className="logo">Google Books</a>
             <ul>
               <li>
-                <Link to="/search">Search</Link>
+                <Link to="/search" className="nav-link">Search</Link>
               </li>
               <li>
-                <Link to="/saved">Saved</Link>
+                <Link to="/saved" className="nav-link">Saved</Link>
               </li>
             </ul>
           </nav>
+          <div className="banner">
+            <h1>(React) Google Books Search</h1>
+            <p>Search for and Save Books of Interest.</p>
+          </div>
           <Switch>
             <Route path="/search">
               <Search />
--- frontend/src/components/Search/index.js
+++ frontend/src/components/Search/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Search() {
     return (
         <div className="Search">
-            
+            <p>search</p>
         </div>
     )
 }
